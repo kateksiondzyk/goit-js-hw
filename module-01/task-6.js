90dc1ac1f70df517aabb39dbc7a77c45a0e0ede9
kate ksiondzyk <kateksiondzyk@gmail.com>
2019-11-15 23:42:52 +0200

    addded task-6

--- module-01/task-6.js
+++ module-01/task-6.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let total = 0;
+
+let input = '';
+
+do {
+    input = prompt('Введите число!', '');
+    input = Number(input);
+    total += input;
+
+} while (input === null);
+
+console.log(`Общая сумма чисел равна ${total}`);
